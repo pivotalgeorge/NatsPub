e5475271add5c59621f6ed2f272c33c321de8e22
George Gelashvili <ggelashvili@vmware.com>
2021-06-03 21:41:04 +0000

    Add log and counter url params to /publish
    for NatsPub-side message visibility and easier tracking of lost messages

--- src/main/java/com/jgainey/natspub/demo/PubController.java
+++ src/main/java/com/jgainey/natspub/demo/PubController.java
@@ -27,40 +27,60 @@ public class PubController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/publish", produces = "application/json")
     public ResponseEntity<String> start(@RequestParam(value = "subject", defaultValue = "HelloWorld") String subject,
-                                        @RequestParam(value = "message", defaultValue = "Hello World Message") String message,
+                                        @RequestParam(value = "message", defaultValue = "Hello World Message") String origMessage,
+                                        @RequestParam(value = "counter", defaultValue = "false") boolean appendCounter,
+                                        @RequestParam(value = "log", defaultValue = "false") boolean logMessages,
                                         @RequestParam(value = "numMessages", defaultValue = "100") long numMessages,
                                         @RequestParam(value = "sleepTime", defaultValue = "100") long sleepTime){
-       boolean toSleep = true;
-
-        if(sleepTime == 0){
-            toSleep = false;
-        }
+        String messageToPublish = origMessage;
 
         if(numMessages == -1){
+            int i = 0;
             while(true){
-                natsPub.publish(subject,message);
-                if(toSleep){
-                    try {
-                        Thread.sleep(sleepTime);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                if(appendCounter) {
+                    messageToPublish = origMessage + " counter: " + i;
+                    i++;
                 }
+                natsPub.publish(subject, messageToPublish);
+                if(logMessages) {
+                    Utils.logInfo("published message: " + messageToPublish);
+                }
+                trySleep(sleepTime);
             }
-        }else{
+        } else {
             for(int i = 0; i < numMessages; i++){
-                natsPub.publish(subject,message);
-                if(toSleep){
-                    try {
-                        Thread.sleep(sleepTime);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                if(appendCounter) {
+                    messageToPublish = origMessage + " counter: " + i;
                 }
+                natsPub.publish(subject, messageToPublish);
+                if(logMessages) {
+                    Utils.logInfo("published message: " + messageToPublish);
+                }
+                trySleep(sleepTime);
             }
         }
 
         return new ResponseEntity<>("ok", HttpStatus.OK);
 
+    }
+    // private String messageWithCounter(boolean count, String message) {
+    //     if (toTimestamp) {
+    //       return "
+    //     )
+    //     return message;
+    // }
+
+    private void trySleep(long sleepTime) {
+        boolean toSleep = true;
+        if(sleepTime == 0){
+            toSleep = false;
+        }
+        if(toSleep){
+            try {
+                Thread.sleep(sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
